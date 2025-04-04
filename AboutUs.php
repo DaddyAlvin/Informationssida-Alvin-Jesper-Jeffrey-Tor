71abe6e7cf031e8f717eb0bca961fb346c023be8
MannenIdenGulaHatten <tornaster433@gmail.com>
2025-04-04 14:53:43 +0200

    Add files via upload
    about us page

---
+++ AboutUs.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="vårlåda">
+        <h1>Vilka är vi?</h1>
+        <p>Vi är en grupp av unga och engagerade killar som strävar efter att få fram bra sidor för våra användare</p>
+    </div>
+
+        
+    
+</body>
+</html>
