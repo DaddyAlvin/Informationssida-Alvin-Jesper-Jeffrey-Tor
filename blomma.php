39fd54bcf916e1bb806d6427551db3269385d3a0
Alvin <alvin.sandgren@elev.ga.ntig.se>
2025-04-21 21:04:57 +0200

    merga index.html här

--- blomma.php
+++ blomma.php
@@ -16,24 +16,110 @@ if (mysqli_connect_errno()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blommor - Meny</title>
+    <link rel="stylesheet" href="index.css">
 
+<header>
+        <div class="top-bar">
+                <div class="menu-icon" onclick="toggleMenu('hamburgerMenu', 0)">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <ul id="hamburgerMenu" class="nav-menu">
+                    <?php
+                    $slaktradQuery = "SELECT * FROM slaktrad";
+                    $slaktradResult = mysqli_query($conn, $slaktradQuery);
 
-    <style>
+                    while ($slakt = mysqli_fetch_assoc($slaktradResult)) {
+                        $slaktID = $slakt['ID'];
+                        $slaktTyp = htmlspecialchars($slakt['typ']);
+                        echo "<li><a href='#' onclick=\"toggleMenu('slakt_$slaktID', 1)\">$slaktTyp</a>";
+                        echo "<ul id='slakt_$slaktID' class='submenu level-1'>";
+
+                        $fargQuery = "SELECT DISTINCT farg.ID, farg.farg FROM farg
+                                      JOIN blomma_farg ON farg.ID = blomma_farg.farg_id
+                                      JOIN blomma ON blomma.ID = blomma_farg.blomma_id
+                                      JOIN blomma_slaktrad ON blomma.ID = blomma_slaktrad.blomma_id
+                                      WHERE blomma_slaktrad.slaktrad_id = $slaktID";
+                        $fargResult = mysqli_query($conn, $fargQuery);
+
+                        while ($farg = mysqli_fetch_assoc($fargResult)) {
+                            $fargID = $farg['ID'];
+                            $fargNamn = htmlspecialchars($farg['farg']);
+                            echo "<li><a href='#' onclick=\"toggleMenu('farg_{$slaktID}_{$fargID}', 2)\">$fargNamn</a>";
+                            echo "<ul id='farg_{$slaktID}_{$fargID}' class='submenu level-2'>";
+
+                            $blommaQuery = "SELECT blomma.ID, blomma.blomma FROM blomma
+                                            JOIN blomma_farg ON blomma.ID = blomma_farg.blomma_id
+                                            JOIN blomma_slaktrad ON blomma.ID = blomma_slaktrad.blomma_id
+                                            WHERE blomma_farg.farg_id = $fargID AND blomma_slaktrad.slaktrad_id = $slaktID";
+                            $blommaResult = mysqli_query($conn, $blommaQuery);
+
+                            while ($blomma = mysqli_fetch_assoc($blommaResult)) {
+                                $blommaID = $blomma['ID'];
+                                $blommaNamn = htmlspecialchars($blomma['blomma']);
+                                echo "<li><a href='visa_blomma.php?blomma_id=$blommaID'>$blommaNamn</a></li>";
+                            }
+
+                            echo "</ul></li>";
+                        }
+
+                        echo "</ul></li>";
+                    }
+                    ?>
+                </ul>
+                <nav class="top-nav">
+                <a href="blomma.php">Hem</a>
+                <a href="AboutUs.php">Om oss</a>
+            </nav>
+            </div>
+            
+        </div>
+        
+    </header>
+
+<main>
+        <section class="row">
+            <div class="flex-box">
+                <p>Rosen är en symbol för kärlek och skönhet. Dess röda färg representerar passion, medan de mjuka kronbladen speglar ömhet och romantik.</p>
+            </div>
+            <div class="flex-box">
+                <p>Rosor finns i många färger – varje färg har sin betydelse. Vita rosor står för oskuld, rosa för tacksamhet, och gula rosor symboliserar vänskap. Det sista här är ett test för att se hur den reagarerar när 2 rader är olika såklart, bäst att vara säker walla frfr 100%</p>
+            </div>
+        </section>
+
+        <section class="poem">
+            <div class="flex-box wide">
+                <p>En ros så röd i morgonens sken,  doftar av drömmar, ömhet och sten.  Med taggar av sorg och blad av hopp,  växer den tyst och ger aldrig opp.</p>
+            </div>
+        </section>
+
+        <section class="row">
+            <div class="flex-box">
+                <p>Rosen har odlats i tusentals år och används ofta i både parfymer, ceremonier och som dekoration i trädgårdar världen över.</p>
+            </div>
+            <div class="flex-box">
+                <p><strong>Någon ful fucking bild</strong><p>
+            </div>
+        </section>
+    </main>
+
+<style>
+
 body {
     font-family: Arial, sans-serif;
     background-color: #f4f4f9;
+    background-image: url('bilder/blommonsterbkg.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
     margin: 0;
     padding: 0;
+    color: black;
 }
 
 a {
     text-decoration: none;
-}
-
-nav {
-    background-color: #333;
-    color: white;
-    padding: 10px;
 }
 
 button {
@@ -48,48 +134,6 @@ button {
 
 button:hover {
     background-color: #444;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-}
-
-li {
-    margin: 5px 0;
-}
-
-.submenu {
-    display: none;
-    margin-left: 20px;
-    background-color: #444;
-    border-radius: 4px;
-}
-
-.submenu a {
-    color: white;
-    padding: 5px;
-    display: block;
-}
-
-.submenu a:hover {
-    background-color: #555;
-}
-
-.submenu.level-0 {
-    background-color: #333;
-}
-
-body {
-    margin: 0;
-    padding: 0;
-    font-family: Arial, sans-serif;
-    background-image: url('bilder/blommonsterbkg.png');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    color: black;
 }
 
 .top-bar {
@@ -123,7 +167,6 @@ body {
 }
 
 .top-nav a {
-    text-decoration: none;
     font-size: 24px;
     font-weight: bold;
     color: black;
@@ -165,6 +208,39 @@ body {
     display: block;
 }
 
+ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+li {
+    margin: 5px 0;
+}
+
+.submenu {
+    display: none;
+    margin-left: 20px;
+    background-color: #444;
+    border-radius: 4px;
+}
+
+.submenu.level-0 {
+    background-color: #333;
+}
+
+.submenu a {
+    color: white;
+    padding: 5px;
+    display: block;
+}
+
+.submenu a:hover {
+    background-color: #333;
+    color: black;
+    font-weight: bold;
+}
+
 main {
     padding: 40px 30px;
 }
@@ -204,60 +280,10 @@ main {
 .wide {
     width: 90%;
 }
-    </style>
+</style>
+
 </head>
 <body>
-
-<a href="blomma.php" style="display:block; padding:10px; color:#333;">Tillbaka till grundfilen</a>
-<a href="AboutUs.php" style="display:block; padding:10px; color:#333;">Till om oss sida</a>
-
-<nav>
-    <button onclick="toggleMenu('hamburgerMenu', 0)">☰ Menu</button>
-    <ul id="hamburgerMenu" class="submenu level-0">
-        <?php
-        $slaktradQuery = "SELECT * FROM slaktrad";
-        $slaktradResult = mysqli_query($conn, $slaktradQuery);
-
-        while ($slakt = mysqli_fetch_assoc($slaktradResult)) {
-            $slaktID = $slakt['ID'];
-            $slaktTyp = htmlspecialchars($slakt['typ']);
-            echo "<li><a href='#' onclick=\"toggleMenu('slakt_$slaktID', 1)\">$slaktTyp</a>";
-            echo "<ul id='slakt_$slaktID' class='submenu level-1'>";
-
-            $fargQuery = "SELECT DISTINCT farg.ID, farg.farg FROM farg
-                          JOIN blomma_farg ON farg.ID = blomma_farg.farg_id
-                          JOIN blomma ON blomma.ID = blomma_farg.blomma_id
-                          JOIN blomma_slaktrad ON blomma.ID = blomma_slaktrad.blomma_id
-                          WHERE blomma_slaktrad.slaktrad_id = $slaktID";
-            $fargResult = mysqli_query($conn, $fargQuery);
-
-            while ($farg = mysqli_fetch_assoc($fargResult)) {
-                $fargID = $farg['ID'];
-                $fargNamn = htmlspecialchars($farg['farg']);
-                echo "<li><a href='#' onclick=\"toggleMenu('farg_{$slaktID}_{$fargID}', 2)\">$fargNamn</a>";
-                echo "<ul id='farg_{$slaktID}_{$fargID}' class='submenu level-2'>";
-
-                $blommaQuery = "SELECT blomma.ID, blomma.blomma FROM blomma
-                                JOIN blomma_farg ON blomma.ID = blomma_farg.blomma_id
-                                JOIN blomma_slaktrad ON blomma.ID = blomma_slaktrad.blomma_id
-                                WHERE blomma_farg.farg_id = $fargID AND blomma_slaktrad.slaktrad_id = $slaktID";
-                $blommaResult = mysqli_query($conn, $blommaQuery);
-
-                while ($blomma = mysqli_fetch_assoc($blommaResult)) {
-                    $blommaID = $blomma['ID'];
-                    $blommaNamn = htmlspecialchars($blomma['blomma']);
-                    echo "<li><a href='visa_blomma.php?blomma_id=$blommaID'>$blommaNamn</a></li>";
-                }
-
-                echo "</ul></li>";
-            }
-
-            echo "</ul></li>";
-        }
-        ?>
-    </ul>
-</nav>
-
 
 <script>
 function toggleMenu(id, level) {
